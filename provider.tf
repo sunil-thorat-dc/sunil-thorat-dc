1dbe9751163eb8c8135612415ed50846bd6bdfcd
sunil-thorat-dc <101177693+sunil-thorat-dc@users.noreply.github.com>
2024-06-20 13:51:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.29.1"
+      version = "5.30"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
