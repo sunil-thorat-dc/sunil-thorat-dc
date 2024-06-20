e1680a5e41df0e7d6ce0dcd05883241c37a32705
sunil-thorat-dc <101177693+sunil-thorat-dc@users.noreply.github.com>
2024-06-20 13:27:32 +0530

    added  provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.29.1"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.28.0"
+    }
+  }
+}
